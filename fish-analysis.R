339685b0b31804c586df68be7bf8042e1cb128e4
Isabella Colpo <isabellacolpo@Isabellas-MacBook-Pro.local>
2018-05-11 17:53:35 -0700

    Modified the plot to create a histogram and to look at narrower ranges of scale size classes by setting the bins argument to 80.

--- fish-analysis.R
+++ fish-analysis.R
@@ -8,3 +8,7 @@ fish_data_cat_filter <- filter(fish_data_cat, scalelength > 1)
 library(tidyverse)
 ggplot(data = fish_data_cat_filter) + 
 geom_point(mapping = aes(x = length, y = scalelength, color = lakeid))
+#Plot histogram of scale length by fish categorical size
+ggplot(fish_data_cat, aes(x = scalelength, fill = length_cat)) +
+  geom_histogram(mapping= aes(x =  scalelength, fill = length_cat), geom ="bar", position = "stack", binwidth = NULL, bins = 80)
+ggsave("scale_hist_by_length.jpg", plot = last_plot())
